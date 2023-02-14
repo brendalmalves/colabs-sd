78e3bf81cb4e5843e46b98c5c7608b162d2b154e
Brenda Alves <brenda.alves@ccc.ufcg.edu.br>
2023-02-14 09:18:31 -0300

    corrigindo forma de cifrar

--- criptografia-simetrica/criptografia.py
+++ criptografia-simetrica/criptografia.py
@@ -1,30 +1,31 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import csv
-import binascii
 
 secret_key = bytes.fromhex('afc70a3cd0a818c0a00217dac82ec008')
 nonce = bytes.fromhex('52f5eda6606c7c239930007f8bf1b18f')
 cipher = Cipher(algorithms.Camellia(secret_key), modes.CTR(nonce))
 
+decryptor = cipher.decryptor()
+encryptor = cipher.encryptor()
+
 def decryption(cipher_text):
-    decryptor = cipher.decryptor()
-    return decryptor.update(bytes.fromhex(cipher_text)) + decryptor.finalize()
+    return decryptor.update(bytes.fromhex(cipher_text)).decode()
 
 def encryption(plan_text):
-    encryptor = cipher.encryptor()
-    return encryptor.update(plan_text) + encryptor.finalize()
+    return encryptor.update(str(plan_text).encode()).hex()
 
 columns_sum = []
-with open('./119111113.csv', 'r') as file:
+with open('./119111113.csv') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
-        column_one = decryption(row[0])
-        column_two =  decryption(row[1])
-        columns_sum.append(int.from_bytes(column_one, byteorder='big') + int.from_bytes(column_two, byteorder='big'))
+        column_one =  int(decryption(row[0]))
+        column_two =  int(decryption(row[1]))
+        sum = column_one + column_two
+        columns_sum.append(encryption(sum))
+
+    encryptor.finalize()
+    decryptor.finalize()
+
 
 with open('./119111113_result.csv', 'w') as file:
-    for value in columns_sum:
-        encrypt_value = encryption(int(value).to_bytes(2, byteorder='big'))
-        writer = csv.writer(file)
-        result =  binascii.hexlify(encrypt_value)
-        writer.writerow([result.decode()])
+    file.write("\n".join(columns_sum))
